691adeee44b730e1b9019cdbdd8332de61c9cd38
Saoud <saoud@saoud.dev>
2021-05-09 15:48:20 -0700

    using appsettings.json instead of MySqlConnector
    Co-Authored-by Salim Mayan <mailsalim@gmail.com>
    Co-authored-by: Collin Chapman <cchap14@gmail.com>

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,26 +3,32 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList
 {
   public class Startup
   {
-    public Startup(IWebHostEnvironment env)
-    {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
-    }
+ public Startup(IWebHostEnvironment env)
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json"); //this line replaces .AddEnvironmentVariables();
+    Configuration = builder.Build();
+}
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set; }
 
     public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddMvc();
-    }
+{
+    services.AddMvc();
 
+    //New code
+    services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+}
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
@@ -41,8 +47,8 @@ namespace ToDoList
       });
     }
   }
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
-  }
+  // public static class DBConfiguration
+  // {
+  //   public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  // }
 }
